486a65139a1d08f6e98d792ea4a9fbbd36c248b2
Jarod <therealfugu@gmail.com>
2021-12-14 14:54:45 -0700

    add simpecov

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in polyn.gemspec
 gemspec
 
+gem "json-schema"
 gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 gem "rubocop", "~> 1.7"
-gem "json-schema"
+gem "simplecov"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require "polyn"
+require "simplecov"
 
 RSpec.configure do |config|
+  SimpleCov.start
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
