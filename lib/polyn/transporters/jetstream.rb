58d9461b4d8a9ae5d78ec9bc7b046f8cc03eaf01
Jarod <therealfugu@gmail.com>
2022-04-14 14:21:56 -0600

    initial jetstream transporter

---
+++ lib/polyn/transporters/jetstream.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+# Copyright 2021-2022 Spiff, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+require "nats/client"
+
+module Polyn
+  module Transporters
+    ##
+    # Jetstream NATS transporter.
+    class Jetstream < Base
+      ##
+      # @private
+      class Envelope < Transporters::Envelope
+        def initialize(topic, msg)
+          super(topic, msg.data)
+          @msg = msg
+        end
+
+        def acknowledge
+          msg.ack
+        end
+
+        private
+
+        attr_reader :msg
+      end
+
+      ##
+      # @private
+      class Subscription < Concurrent::Actor::RestartingContext
+        include SemanticLogger::Loggable
+
+        ##
+        # @private
+        class Wrapper
+          def iniitalize(actor)
+            @actor = actor
+          end
+        end
+
+        def self.spawn(sub, transit)
+          Wrapper.call(super(sub, transit))
+        end
+
+        def initialize(sub, transit)
+          super
+          @sub     = sub
+          @transit = transit
+
+          sub.fetch(1) do |messages|
+            messages.each do |message|
+              tx_message = Envelope.new(sub.subject, message)
+              transit << [:receive, tx_message]
+            end
+          end
+        end
+
+        private
+
+        attr_reader :sub, :transit
+      end
+
+      def initialize(transit, options = {})
+        super
+        @subscriptions = []
+      end
+
+      def connect
+        logger.info("connecting")
+        @nats   = NATS.connect(options)
+        @client = nats.jetstream
+      rescue Errno::ECONNREFUSED => e
+        logger.error("connect failed: #{e}")
+        raise Polyn::Transporters::Errors::TimeoutError, e
+      end
+
+      def disconnect
+        logger.info("disconnecting")
+      end
+
+      private
+
+      attr_reader :nats
+    end
+  end
+end
---
+++ spec/polyn/transporters/jetstream_spec.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+# Copyright 2021-2022 Spiff, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+require "spec_helper"
+require_relative "../../helpers/transporters"
+require_relative "../../../lib/polyn/transporters/jetstream"
+
+RSpec.describe Polyn::Transporters::Jetstream do
+  include_examples "a transporter" do
+    let(:options) do
+      "nats://localhost:4222"
+    end
+
+    let(:nats_client) { NATS.connect(options).jetstream }
+
+    # before :each do
+    #   nats_client.add_stream(name: "test_stream", subjects: ["test-topic"])
+    # end
+
+    after :each do
+      # nats_client.subscription("test-test-topic")&.delete
+      # nats_client.topic("test-topic")&.delete
+    end
+
+    describe "#connect!" do
+      it "should raise Polyn::Transporters::Errors::TimeoutError when a time out occurs during connect" do
+        expect_any_instance_of(NATS::IO::Client).to receive(:connect).and_raise(Errno::ECONNREFUSED)
+
+        expect { subject.connect! }.to raise_exception(Polyn::Transporters::Errors::TimeoutError)
+      end
+
+      it "should connect to the nats instance without exception when a timeout does not occur" do
+        expect { subject.connect! }.not_to raise_exception
+      end
+    end
+
+    describe "#publish" do
+      before :each do
+        subject.connect!
+
+        topic   = nats_client.topic("test-topic")
+        nats_client.create_topic("test-topic") unless topic
+      end
+
+      after :each do
+        subject.disconnect!
+
+        nats_client.topic("test-topic")&.delete
+      end
+
+      it "should raise Polyn::Transporters::Errors::TimeoutError when a time out occurs during publish" do
+        expect_any_instance_of(Google::Cloud.nats::Topic).to receive(:publish)
+          .and_raise(Google::Cloud::DeadlineExceededError)
+
+        expect do
+          subject.publish!("test-topic",
+            "test-message")
+        end.to raise_exception(Polyn::Transporters::Errors::TimeoutError)
+      end
+
+      it "should publish a message to the nats instance without exception when a timeout does not occur" do
+        expect do
+          subject.publish!("test-topic",
+            "test-message")
+        end.not_to raise_exception
+      end
+    end
+
+    describe "#subscribe" do
+      it "should raise Polyn::Transporters::Errors::TimeoutError when a time out occurs during subscribe" do
+        expect_any_instance_of(Google::Cloud.nats::Subscription).to receive(:listen)
+          .and_raise(Google::Cloud::DeadlineExceededError)
+
+        expect do
+          subject.subscribe!("test", "test-topic")
+        end.to raise_exception(Polyn::Transporters::Errors::TimeoutError)
+      end
+
+      it "raises a Polyn::Transporters::Errors::TopicNotFoundError when the topic does not exist" do
+        expect do
+          subject.subscribe!("test", "test-non-topic")
+        end.to raise_exception(Polyn::Transporters::Errors::TopicNotFoundError)
+      end
+
+      it "should subscribe to the nats instance without exception when a timeout does not occur" do
+        expect do
+          subject.subscribe!("test", "test-topic")
+        end.not_to raise_exception
+      end
+    end
+  end
+end
