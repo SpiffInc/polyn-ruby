cb053329ca0c833ea522f3eb01b655f6e170f49c
brandynbennett <brandynlbennett@gmail.com>
2022-08-02 16:59:36 -0600

    add cloud event json and class to read it

---
+++ spec/polyn/cloud_event_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "polyn/cloud_event"
+
+RSpec.describe Polyn::CloudEvent do
+  describe "#to_h" do
+    it "returns the cloud event schema as a hash" do
+      expect(described_class.to_h).to be_a(Hash)
+    end
+  end
+end
