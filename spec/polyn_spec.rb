78a7b86e8b5873e56386737d9d75dcabf54065f4
brandynbennett <brandynlbennett@gmail.com>
2022-08-04 10:40:26 -0600

    get publish spec more focused

--- spec/polyn_spec.rb
+++ spec/polyn_spec.rb
@@ -18,7 +18,10 @@ RSpec.describe Polyn do
   end
 
   describe "#publish" do
-    it "publishes a message" do
+    before(:each) do
+      js.add_stream(name: "CALC", subjects: ["calc.mult.v1"])
+      js.add_consumer("CALC", durable_name: "my_consumer")
+
       add_schema("calc.mult.v1", {
         "type"       => "object",
         "properties" => {
@@ -31,48 +34,29 @@ RSpec.describe Polyn do
           },
         },
       })
+    end
 
-      js.add_stream(name: "CALC", subjects: ["calc.mult.v1"])
-      js.add_consumer("CALC", durable_name: "my_consumer")
+    after(:each) do
+      js.delete_stream("CALC")
+    end
 
+    it "publishes a message" do
       Polyn.publish(nats, "calc.mult.v1", {
         a: 1,
         b: 2,
       }, store_name: store_name)
 
-      psub = js.pull_subscribe("calc.mult.v1", "my_consumer", stream: "CALC")
+      msg = get_message("calc.mult.v1", "my_consumer", "CALC")
 
-      msgs = psub.fetch(1)
-      msgs.each do |msg|
-        event = JSON.parse(msg.data)
-        expect(msg.subject).to eq("calc.mult.v1")
-        expect(event["type"]).to eq("com.test.calc.mult.v1")
-        expect(event["source"]).to eq("com:test:user:backend")
-        expect(event["data"]["a"]).to eq(1)
-        expect(event["data"]["b"]).to eq(2)
-        msg.ack
-      end
-
-      js.delete_stream("CALC")
+      event = JSON.parse(msg.data)
+      expect(msg.subject).to eq("calc.mult.v1")
+      expect(event["type"]).to eq("com.test.calc.mult.v1")
+      expect(event["source"]).to eq("com:test:user:backend")
+      expect(event["data"]["a"]).to eq(1)
+      expect(event["data"]["b"]).to eq(2)
     end
 
     it "adds triggered_by to polyntrace" do
-      add_schema("calc.mult.v1", {
-        "type"       => "object",
-        "properties" => {
-          "data" => {
-            "type"       => "object",
-            "properties" => {
-              "a" => { "type" => "integer" },
-              "b" => { "type" => "integer" },
-            },
-          },
-        },
-      })
-
-      js.add_stream(name: "CALC", subjects: ["calc.mult.v1"])
-      js.add_consumer("CALC", durable_name: "my_consumer")
-
       first_event = Polyn::Event.new({ type: "first.event", data: "foo" })
 
       Polyn.publish(nats, "calc.mult.v1", {
@@ -80,38 +64,19 @@ RSpec.describe Polyn do
         b: 2,
       }, store_name: store_name, triggered_by: first_event)
 
-      psub = js.pull_subscribe("calc.mult.v1", "my_consumer", stream: "CALC")
+      msg = get_message("calc.mult.v1", "my_consumer", "CALC")
 
-      msgs = psub.fetch(1)
-      msgs.each do |msg|
-        event = JSON.parse(msg.data)
-        expect(event["polyntrace"]).to eq([
-                                            {
-                                              "id"   => first_event.id,
-                                              "type" => first_event.type,
-                                              "time" => first_event.time,
-                                            },
-                                          ])
-        msg.ack
-      end
-
-      js.delete_stream("CALC")
+      event = JSON.parse(msg.data)
+      expect(event["polyntrace"]).to eq([
+                                          {
+                                            "id"   => first_event.id,
+                                            "type" => first_event.type,
+                                            "time" => first_event.time,
+                                          },
+                                        ])
     end
 
     it "raises if msg doesn't conform to schema" do
-      add_schema("calc.mult.v1", {
-        "type"       => "object",
-        "properties" => {
-          "data" => {
-            "type"       => "object",
-            "properties" => {
-              "a" => { "type" => "integer" },
-              "b" => { "type" => "integer" },
-            },
-          },
-        },
-      })
-
       expect do
         Polyn.publish(nats, "calc.mult.v1", {
           a: "1",
@@ -123,5 +88,13 @@ RSpec.describe Polyn do
 
   def add_schema(type, schema)
     Polyn::SchemaStore.save(nats, type, schema, name: store_name)
+  end
+
+  def get_message(type, consumer, stream)
+    psub = js.pull_subscribe(type, consumer, stream: stream)
+
+    msgs = psub.fetch(1)
+    msgs.each(&:ack)
+    msgs[0]
   end
 end
