6e4706c064c45f9bc398fa9426b4e59e78dc90ff
brandynbennett <brandynlbennett@gmail.com>
2022-08-10 16:19:04 -0600

    schema_store has get and get!

--- lib/polyn/schema_store.rb
+++ lib/polyn/schema_store.rb
@@ -19,19 +19,28 @@ module Polyn
       JSONSchemer.schema(schema)
     end
 
+    def self.get!(nats, type, **opts)
+      result = get(nats, type, **opts)
+      raise result if result.is_a?(Polyn::Errors::SchemaError)
+
+      result
+    end
+
     def self.get(nats, type, **opts)
       kv    = nats.jetstream.key_value(store_name(**opts))
       entry = kv.get(type)
       entry.value
     rescue NATS::KeyValue::BucketNotFoundError
-      raise Polyn::Errors::SchemaError,
+      Polyn::Errors::SchemaError.new(
         "The Schema Store has not been setup on your NATS server. Make sure you use "\
-        "the Polyn CLI to create it"
+        "the Polyn CLI to create it",
+      )
     rescue NATS::JetStream::Error::NotFound
-      raise Polyn::Errors::SchemaError,
+      Polyn::Errors::SchemaError.new(
         "Schema for #{type} does not exist. Make sure it's "\
         "been added to your `events` codebase and has been loaded "\
-        "into the schema store on your NATS server"
+        "into the schema store on your NATS server",
+      )
     end
 
     def self.store_name(**opts)
